7362b1ecec441d9ca76a8c577516d5b7300dfcf9
shan1117 <Shandrokumar2023@gmail.com>
2025-12-02 18:38:59 +0530

    Add print statement for file edit notification

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 def main():
     print("Hello from test-proj!")
     print("running pandas version: ", pd.__version__)
+    print("github.com mein edit hua file!")
 
 
 if __name__ == "__main__":
